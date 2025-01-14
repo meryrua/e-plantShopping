824c6f99550b3dc2a916f8d2c69a388614ca02f1
Natalia <meryrua@gmail.com>
2025-01-14 06:03:10 -0500

    Add cost and description

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -278,6 +278,9 @@ const handlePlantsClick = (e) => {
                       <img className="product-image" src={plant.image} alt={plant.name} />
                       <div className="product-title">{plant.name}</div>
                       <div className="product-price">${plant.cost}</div>
+                      <div>
+                        <em> {plant.description}</em>
+                      </div>
                       {/*Similarly like the above plant.name show other details like description and cost*/}
                       <button  className="product-button" onClick={() => handleAddToCart(plant)}>Add to Cart</button>
                     </div>
